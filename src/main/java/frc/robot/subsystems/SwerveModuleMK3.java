ea88f69e9bf047d18bb7e5df3e1fc0287a8c3428
Jason Rubenstein <jruben4@yahoo.com>
2021-12-02 22:06:48 -0600

    Fixing optimizer?  Needs testing

--- src/main/java/frc/robot/subsystems/SwerveModuleMK3.java
+++ src/main/java/frc/robot/subsystems/SwerveModuleMK3.java
@@ -350,9 +350,9 @@ public class SwerveModuleMK3 {
    * @param desiredState - A SwerveModuleState representing the desired new state
    *                     of the module
    */
-  public void setDesiredState(SwerveModuleState state) {
+  public void setDesiredState(SwerveModuleState m_state) {
 
-    SwerveModuleState.optimize(state, Rotation2d.fromDegrees(m_internalAngle)); // should favor reversing direction over
+    SwerveModuleState state = SwerveModuleState.optimize(m_state, Rotation2d.fromDegrees(m_internalAngle)); // should favor reversing direction over
                                                                                 // turning > 90 degrees
 
     // use position control on angle with INTERNAL encoder, scaled internally for
